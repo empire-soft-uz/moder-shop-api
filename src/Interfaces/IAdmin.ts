b9478776e6c1ad6ea14b8ca83dad79ba08e988e7
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-12 16:55:09 +0500

    created admin model

---
+++ src/Interfaces/IAdmin.ts
@@ -0,0 +1,6 @@
+export default interface IAdmin {
+  id: string;
+  email: string;
+  password: string;
+  vendorId: string;
+}
---
+++ src/Models/Admin.ts
@@ -0,0 +1,35 @@
+import { Model, Schema, model } from "mongoose";
+interface admin {
+  email: string;
+  password: string;
+  vendorId: string;
+}
+interface AdminDoc extends Document {
+  email: string;
+  password: string;
+  vendorId: string;
+}
+interface AdminModel extends Model<AdminDoc> {
+  build(attrs: admin): AdminDoc;
+}
+
+const adminSchema = new Schema(
+  {
+    email: String,
+    password: String,
+    vendorId: String,
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+adminSchema.statics.build = (attrs: admin): AdminDoc => {
+  return new Admin(attrs);
+};
+const Admin = model<AdminDoc, AdminModel>("Admin", adminSchema);
