ff491e7745325915b5cf7ce56e05e3e122a6dcb1
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-13 18:45:11 +0500

    completed Product interface

--- src/Interfaces/Product/IProduct.ts
+++ src/Interfaces/Product/IProduct.ts
@@ -1,8 +1,11 @@
 import IVendor from "../Vendor/IVendor";
+import IPrice from "./IPrice";
 
 export default interface IProduct {
   id: string;
   vendorId: IVendor["id"];
   name: string;
   description: string;
+  price: Array<IPrice> | IPrice;
+  media: Array<string>;
 }
