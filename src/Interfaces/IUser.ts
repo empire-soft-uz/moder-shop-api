1f701801cb04ecbe92dd88bdd4cabd6451632e83
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-14 15:20:47 +0500

    added id to user interface

--- src/Interfaces/IUser.ts
+++ src/Interfaces/IUser.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export default interface IUser {
-  //id: mongoose.Types.ObjectId;
+  id: string;
   fullName: string;
   password: string;
   phoneNumber: number;
--- src/Interfaces/Product/IReview.ts
+++ src/Interfaces/Product/IReview.ts
@@ -1,6 +1,8 @@
+import IUser from "../IUser";
 import IProductMedia from "./IProducMedia";
 
 export default interface IReview {
+  authorId: IUser["id"];
   id: string;
   createdDate: Date;
   review: string;
