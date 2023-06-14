220950c8cdc128b0b90b3b7749c356d146f845cd
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-14 18:14:22 +0500

    added async error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import userRoute from "./routes/userRoute";
+import errorHandler from "./middlewares/errorHandler";
+
+import "express-async-errors";
+
 const app = express();
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
+  throw new Error("async error");
   res.send("test message");
 });
 app.use("/api/users", userRoute);
 
+app.all("*", (req, res, next) => {
+  res.status(404).send("Route Not Found");
+});
+app.use(errorHandler);
 export default app;
