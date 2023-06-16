d2dae3d5345fb58588730682bffd47d96db2cf54
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-16 09:36:47 +0500

    updated error midleware

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,14 @@
-import { NextFunction } from "express";
+import BaseError from "../Classes/Errors/BaseError";
+
 //@ts-ignore
-export default function errorHandler(err, req, res, next) {
-  console.log(err);
-  res.status(500).send({ errors: [{ message: err.message }] });
-}
+const errorHandler = (err, req, res, next) => {
+  console.log("error handling middleware", err);
+  if (err instanceof BaseError) {
+    res
+      .status(err.statusCode)
+      .send({ message: err.message, errors: err.formatError() });
+    return;
+  }
+  res.status(500).send({ message: "Something went wrong", errors: [err] });
+};
+export default errorHandler;
