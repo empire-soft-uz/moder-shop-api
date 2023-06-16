a216f4274c07b395d190cbacaf78419836269f39
Akbarjon Abdukarimov <akbarjonabdukarimov6865@gmail.com>
2023-06-16 09:52:30 +0500

    declared validation results for user registration

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,7 +1,34 @@
 import { Router } from "express";
+import { body, validationResult } from "express-validator";
+import RequestValidationError from "../Classes/Errors/RequestValidationError";
 const userRoute = Router();
+const validationRules = [
+  body("fullName").notEmpty().withMessage("Name is required"),
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required")
+    .isLength({ min: 4, max: 20 })
+    .withMessage(
+      "Password length shoulr be between 4 and 20 characters cantaining capital and lower case letters with numbers"
+    ),
+  body("phoneNumber")
+    .notEmpty()
+    .isMobilePhone("any")
+    .withMessage("Please provide valid phone number"),
+  body("email")
+    .notEmpty()
+    .isEmail()
+    .withMessage("Please provide valid email address"),
+  body("gender").notEmpty().withMessage("Please provide your gender"),
+  body("birthDate").notEmpty().withMessage("Please provide your Date of Birth"),
+];
 
-userRoute.post("/register", async (req, res) => {
+userRoute.post("/register", [...validationRules], async (req, res) => {
+  const errs = validationResult(req);
+  if (!errs.isEmpty) {
+    throw new RequestValidationError(errs.array());
+  }
+
   res.send("register route");
 });
 
